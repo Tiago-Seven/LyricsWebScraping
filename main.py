85b589b8524feff520435485146c12f04fd25d57
TNmieic <up201506203@fe.up.pt>
2018-12-22 18:15:51 +0000

    prettifying raw lyrics output

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup as soup
+import re
 
 #Page URL
-theRingerUrl = "https://genius.com/Eminem-the-ringer-lyrics"
+pageUrl = "https://genius.com/Eminem-greatest-lyrics"
 
 #headers so that genius doesn't return 403
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
-req = Request(url=theRingerUrl, headers=headers)
+req = Request(url=pageUrl, headers=headers)
 uClient = urlopen(req)
 
 pageHtml = uClient.read()
 uClient.close()
 #parse HTML from genius page
 page_soup = soup(pageHtml,"html.parser")
-print(page_soup.h1)
+rawLyrics = page_soup.find("div",class_="lyrics").getText()
+
+pretty = re.sub(r'\[Intro\]|\[Chorus\]|\[Verse \d\]', '', rawLyrics)
+#removed everything except alphanumeric and spaces
+pretty = re.sub(r'([^\s\w]|_)+', '', pretty)
+pretty = re.sub('\n+', " ", pretty)
+print(pretty.split(" "))
