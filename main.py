b799f558d61ab9e13677d92adefa97f569a2f19f
TNmieic <up201506203@fe.up.pt>
2018-12-26 16:05:37 +0000

    added flask to make a restful server

--- main.py
+++ main.py
@@ -3,103 +3,113 @@ from bs4 import BeautifulSoup as soup
 import re
 from nltk.corpus import stopwords
 
+from flask import Flask
+
 #headers so that genius doesn't return 403
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 
-#removes tags in the lyrics 
+#removes tags in the lyrics
 def removeTags(text):
-  #assumption that square brackets never appear on the actual lyrics
-  return re.sub(r'\[(.*?)\]', '', text)
+    #assumption that square brackets never appear on the actual lyrics
+    return re.sub(r'\[(.*?)\]', '', text)
 
 def wordListToLowercase(wordList):
-  wordList = [word.lower() for word in wordList]
-  return wordList
+    wordList = [word.lower() for word in wordList]
+    return wordList
 
 #removes common stop words using nltk https://www.nltk.org/
 def removeStopWords(wordList):
-  wordList = [word for word in wordList if word not in stopwords.words('english')]
-  return wordList
+    wordList = [
+        word for word in wordList if word not in stopwords.words('english')]
+    return wordList
 
 def prettifyRawLyrics(rawLyrics):
-  #remove tags that dont belong with the lyrics
-  prettier = removeTags(rawLyrics)
+    #remove tags that dont belong with the lyrics
+    prettier = removeTags(rawLyrics)
 
-  #removed everything except alphanumeric and spaces
-  prettier = re.sub(r'([^\s\w]|_)+', '', prettier)
+    #removed everything except alphanumeric and spaces
+    prettier = re.sub(r'([^\s\w]|_)+', '', prettier)
 
-  #remove spaces and \n from the ends
-  prettier = prettier.strip()
+    #remove spaces and \n from the ends
+    prettier = prettier.strip()
 
-  #replace \n with " " to separate words
-  prettier = re.sub('\n+', " ", prettier)
+    #replace \n with " " to separate words
+    prettier = re.sub('\n+', " ", prettier)
 
-  return prettier
+    return prettier
 
 def getHTMLSoup(url):
-  req = Request(url=url, headers=headers)
-  uClient = urlopen(req)
+    req = Request(url=url, headers=headers)
+    uClient = urlopen(req)
 
-  #get HTML
-  pageHtml = uClient.read()
-  #close connection
-  uClient.close()
+    #get HTML
+    pageHtml = uClient.read()
+    #close connection
+    uClient.close()
 
-  #get soup HTML from genius page
-  page_soup = soup(pageHtml, "html.parser")
+    #get soup HTML from genius page
+    page_soup = soup(pageHtml, "html.parser")
 
-  return page_soup
+    return page_soup
 
 def addDictionaries(dict1, dict2):
-  for word in dict2:
-    if word in dict1:
-      dict1[word] += dict2[word]
-    else:
-      dict1[word] = dict2[word]
-  return dict1
+    for word in dict2:
+        if word in dict1:
+            dict1[word] += dict2[word]
+        else:
+            dict1[word] = dict2[word]
+    return dict1
 
 def getSongLyricDictionary(url):
-  page_soup = getHTMLSoup(url)
-  rawLyrics = page_soup.find("div",class_="lyrics").getText()
+    page_soup = getHTMLSoup(url)
+    rawLyrics = page_soup.find("div", class_="lyrics").getText()
 
-  #prettify raw lyrics
-  prettier = prettifyRawLyrics(rawLyrics)
+    #prettify raw lyrics
+    prettier = prettifyRawLyrics(rawLyrics)
 
-  #split lyrics to a word list
-  wordList = prettier.split(' ')
-  wordList = wordListToLowercase(wordList)
-  filtered_words = removeStopWords(wordList)
+    #split lyrics to a word list
+    wordList = prettier.split(' ')
+    wordList = wordListToLowercase(wordList)
+    filtered_words = removeStopWords(wordList)
 
-  counts = dict()
-  for i in filtered_words:
-    counts[i] = counts.get(i, 0) + 1
+    counts = dict()
+    for i in filtered_words:
+        counts[i] = counts.get(i, 0) + 1
 
-  return counts
-  
+    return counts
+
 def getAlbumLyricDictionary(url):
-  page_soup = getHTMLSoup(url)
-  row = page_soup.find_all("a", href=True)
-  songsURLs = []
+    page_soup = getHTMLSoup(url)
+    row = page_soup.find_all("a", href=True)
+    songsURLs = []
 
-  counts = dict()
-  for a in row:
-    if a.find("h3", class_="chart_row-content-title"):
-      songsURLs.append(a["href"])
-  for songULR in songsURLs:
-    print(songULR)
-    songDict = getSongLyricDictionary(songULR)
-    print("adding...")
-    counts = addDictionaries(counts, songDict)
-  return counts
+    counts = dict()
+    for a in row:
+        if a.find("h3", class_="chart_row-content-title"):
+            songsURLs.append(a["href"])
+    for songULR in songsURLs:
+        print(songULR)
+        songDict = getSongLyricDictionary(songULR)
+        print("adding...")
+        counts = addDictionaries(counts, songDict)
+    return counts
 
 
-albumURL = "https://genius.com/albums/Eminem/Kamikaze"
-lyricDictionary = getAlbumLyricDictionary(albumURL)
-i = 0
-for w in sorted(lyricDictionary, key=lyricDictionary.get, reverse=True):
-  if(i < 50):
-    print(w, lyricDictionary[w])
-  i += 1
+app = Flask(__name__)
+
+
+@app.route('/album/<string:albumName>')
+def albumStatsRoute(albumName):
+    return albumName
+
+# albumURL = "https://genius.com/albums/Eminem/Kamikaze"
+# lyricDictionary = getAlbumLyricDictionary(albumURL)
+# i = 0
+# for w in sorted(lyricDictionary, key=lyricDictionary.get, reverse=True):
+#     if(i < 50):
+#         print(w, lyricDictionary[w])
+#     i += 1
 
 # dict1 = {"a":1,"b":2,"c":2}
 # dict2 = {"a":1,"c":2,"d":3}
@@ -111,6 +121,6 @@ for w in sorted(lyricDictionary, key=lyricDictionary.get, reverse=True):
 
 #TODO number of words/ number of unique words
 
-#TODO try to add unit tests... 
+#TODO try to add unit tests...
 #addDictionaries
 #prettifyRawLyrics
